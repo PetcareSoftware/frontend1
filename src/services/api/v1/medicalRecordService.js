74a7b8ba0ee4061ed068fb85656b5134cfdbb3ec
Politi23 <jesusilva1023@gmail.com>
2026-06-02 10:58:19 -0400

    Mover get antes de getSummary en MedicalRecordService

--- src/services/api/v1/medicalRecordService.js
+++ src/services/api/v1/medicalRecordService.js
@@ -3,14 +3,14 @@ import api from '@/services/api/v1/api';
 export const PETS_BASE = 'pets/';
 
 export class MedicalRecordService {
-  static async getSummary(petId) {
-    const response = await api.get(`${PETS_BASE}${petId}/medical-record/summary/`);
+  static async get(petId) {
+    const response = await api.get(`${PETS_BASE}${petId}/medical-record/`);
 
     return response.data;
   }
 
-  static async get(petId) {
-    const response = await api.get(`${PETS_BASE}${petId}/medical-record/`);
+  static async getSummary(petId) {
+    const response = await api.get(`${PETS_BASE}${petId}/medical-record/summary/`);
 
     return response.data;
   }
